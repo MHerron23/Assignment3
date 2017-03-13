aab6033e8f6f01caa73435a9685c645e94989812
Michael <Michael@Michael>
2017-03-13 16:18:46 +0000

    bfdhfb

--- Assignment3/main_lights.py
+++ Assignment3/main_lights.py
@@ -1,15 +1,100 @@
-'''
-Created on 11 Mar 2017
 
-@author: Michael
-'''
 import numpy as np
+
 import urllib.request
 import re
 
 
+# Function to validate values are within the given range of grid size.
+# If not present then manipulate the vales according to grid size and then pass on to calculate.
+def validate_values(x1,y1,x2,y2,L):
+
+    
+    
+    
+    #Check for grid values
+    if x2 >= x1 and y2 >= y1:
+    
+    #If values are negative then make them zero and make sure that values are not out of bound.
+    
+        if x1 < 0:
+            x1 = 0 
+        elif x1 >= L:
+            x1 = L-1
+        else:
+            x1 = x1
+             
+        if y1 < 0:
+            y1 = 0
+        elif y1 >= L:
+            y1 = L-1
+            
+        if x2 < 0:
+            x2 = 0
+        elif x2 >= L:
+            x2 = L-1
+            
+        if y2 < 0:
+            y2 = 0
+        elif y2 >= L:
+            y2 = L-1        
+    else:
+        print("Invalid data for grid.")
+    return x1,y1,x2,y2,L
+    
+    
+#Functions to turn lights ON/OFF/SWITCH
 
 
+
+
+#Function to turn lights ON which are OFF
+def turn_lights_ON(x1,y1,x2,y2,ledGrid):
+    
+    for row in range(y1,y2+1):
+        for col in range(x1,x2+1):
+            if ledGrid[row][col] == False:
+                ledGrid[row][col] = True
+    return ledGrid
+
+
+#Function to turn the lights OFF which are ON
+def turn_lights_OFF(x1,y1,x2,y2,ledGrid):
+    
+    for row in range(y1,y2+1):
+        for col in range(x1,x2+1):
+            if ledGrid[row][col] == True:
+                ledGrid[row][col] = False
+    return ledGrid
+
+#Function to toggle values of lights ON->OFF/OFF->ON
+def switch_lights(x1,y1,x2,y2,ledGrid):
+    
+    
+       
+    
+    
+    for row in range(y1,y2+1):
+        for col in range(x1,x2+1):
+            if ledGrid[row][col] == True:
+                ledGrid[row][col] = False
+            elif ledGrid[row][col] == False:
+                ledGrid[row][col] = True
+                
+    return ledGrid
+
+
+#Function to count number of lights that are in ON state.
+def count_lights_ON(ledGrid):
+    count = 0
+    for row in ledGrid:
+        for col in row:
+            if col == True:
+                count += 1
+    return count
+
+
+# Main function which will call above all functions and will take input filename from command prompt.
 def main():
 
     filename = input("Please enter the file name: ")#"http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
@@ -30,13 +115,14 @@ def main():
     L = int(str1) 
     grid = [[False] * L for i in range(L)]
     ledGrid = np.array(grid)
-    
+     
+     
     #save data of the file from web to lights.txt    
     with open ("lights.txt", "w") as text_file:
         text_file.write(filename)
         
     text_file.close()
-    
+      
     print("Calculating...")   
     filename = open("lights.txt", "r")
     x = filename.read()
@@ -47,8 +133,51 @@ def main():
             line = line.replace(',', ' ') 
             
             values = re.sub("[^\w]", " ",  line).split() 
-
-
-
-if __name__ == '__main__':
+            
+            
+            
+            command = values[0] + " " + values[1]
+            if command == "turn on":
+                x1 = int(values[2])
+                y1 = int(values[3])
+                x2 = int(values[5])
+                y2 = int(values[6])
+                 
+                
+                result = validate_values(x1, y1, x2, y2, L)
+                x1 = result[0]
+                y1 = result[1]
+                x2 = result[2]
+                y2 = result[3]
+                ledGrid = turn_lights_ON(x1, y1, x2, y2, ledGrid)
+         
+            elif command == "turn off":
+                x1 = int(values[2])
+                y1 = int(values[3])
+                x2 = int(values[5])
+                y2 = int(values[6])
+                result = validate_values(x1, y1, x2, y2, L)
+                x1 = result[0]
+                y1 = result[1]
+                x2 = result[2]
+                y2 = result[3]
+                ledGrid = turn_lights_OFF(x1, y1, x2, y2, ledGrid)
+                 
+            #Values[0] will be the first item in the line, if it is switch assign values
+            elif values[0] == "switch":
+                x1 = int(values[1])
+                y1 = int(values[2])
+                x2 = int(values[4])
+                y2 = int(values[5])
+                result = validate_values(x1, y1, x2, y2, L)
+                x1 = result[0]
+                y1 = result[1]
+                x2 = result[2]
+                y2 = result[3]
+                ledGrid = switch_lights(x1, y1, x2, y2, ledGrid)
+  
+    print("The number of lights that are in ON state are: ", count_lights_ON(ledGrid))
+        
+    
+if __name__ == "__main__":
     main()
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,7 +1,53 @@
 import unittest
+from Assignment3 import main_lights
+import os.path
+#from code.main_lights import validate_values,turn_lights_ON,turn_lights_OFF,switch_lights,count_lights_ON
+
+class Test(unittest.TestCase):
+    
+    def test_setup(self):
+        pass
+    # Function to check if file is parsed correctly
+    def test_file(self):
+        
+        line = "1000"
+        with open('testing.txt', 'r') as f:
+            first_line = f.readline()
+            values = first_line.strip().split()
+            x = values[0]
+        self.assertEqual(line, x)
+    # Function to check lights that are ON  
+    def test_turn_on(self):
+        
+        self.assertEqual(main_lights.turn_lights_ON(0, 0, 2, 2, [[False, False, False], [False, False, False], [False, False, False]]), [[True, True, True], [True, True, True], [True, True, True]])
+
+    # Function to check that lights are switched OFF
+    def test_turn_off(self):
+        
+        self.assertEqual(main_lights.turn_lights_OFF(0, 0, 2, 2, [[True, True, True], [True, True, True], [True, True, True]]), [[False, False, False], [False, False, False], [False, False, False]])
+
+    #Toggle function for ON and OFF lights.
+    def test_switch(self):
+        
+        self.assertEqual(main_lights.switch_lights(0,0,2,2, [[True, False, True], [True, False, True], [True, False, True]]),[[False, True, False], [False, True, False], [False, True, False]])
+    #Function for data validation
+    def test_validate_values(self):
+        self.assertEqual(main_lights.validate_values(-1, -2, 4, 4, 3), (0,0,2,2,3))
+    #Function to count number of lights that are ON.
+    def test_count_lights_ON(self):
+        
+        self.assertEqual(main_lights.count_lights_ON([[True, False, True], [True, False, True], [True, False, True]]), 6)
+    
+    
+    #Function to check if input file is present
+    def test_input_file(self):
+        
+        
+        self.assertEqual(os.path.isfile("testing.txt"), True)
+
 
     
  
 if __name__ == "__main__":
     
-    pass
+    unittest.main()
